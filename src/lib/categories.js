c90edb72e611b8fac1991842b63118ca6f624191
rikoyudianhar00-afk <rikoyudianhar00@gmail.com>
2026-08-19 09:45:44 +0700

    Create categories.js

---
+++ src/lib/categories.js
@@ -0,0 +1,17 @@
+export const CATEGORIES = [
+  { slug: "top-up", label: "Top Up", color: "#DCEBFF", icon: "diamond" },
+  { slug: "game-key", label: "Game Key", color: "#E4E9FF", icon: "key", badge: "Baru" },
+  { slug: "akun", label: "Akun", color: "#F2E4FF", icon: "hand" },
+  { slug: "voucher", label: "Voucher", color: "#FCE4EE", icon: "cards" },
+  { slug: "joki", label: "Joki", color: "#DFF5E8", icon: "trophy" },
+  { slug: "item", label: "Item", color: "#E7F5D9", icon: "sword" },
+];
+
+export const ICON_PATHS = {
+  diamond: "M6 3h12l3 5-9 13L3 8l3-5Z",
+  key: "M15 7a4 4 0 1 1-4 4H5l-2 2 2 2 2-2h1l2-2",
+  hand: "M8 13V6a1.5 1.5 0 0 1 3 0v5m0-4a1.5 1.5 0 0 1 3 0v4m0-3a1.5 1.5 0 0 1 3 0v5c0 4-2 7-6 7s-6-3-7-5l-1.5-3a1.4 1.4 0 0 1 2.3-1.6L8 14",
+  cards: "M4 6h13v11H4zM9 3h13v11",
+  trophy: "M8 4h8v4a4 4 0 0 1-8 0V4Zm-4 0h4m12 0h-4M9 12v3h6v-3M8 19h8",
+  sword: "m5 19 9-9M17 3l4 4-3 3-4-4 3-3ZM9 15l-2 2-2-1-1-2 2-2 3 3Z",
+};
