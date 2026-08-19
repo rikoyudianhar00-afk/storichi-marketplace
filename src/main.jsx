79a3f87b4c556db4d74a07fb4f87b6180907fab5
rikoyudianhar00-afk <rikoyudianhar00@gmail.com>
2026-08-19 09:49:23 +0700

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import "./index.css";
+import "./styles/layout.css";
+import "./styles/catalog.css";
+import "./styles/social.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
